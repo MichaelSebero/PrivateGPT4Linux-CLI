4f7229fd3f0bb85d64da64bcd2c6ffe5c9c2bac9
Michael Sebero <106393611+MichaelSebero@users.noreply.github.com>
2023-05-30 11:22:43 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,4 +6,6 @@ cd /home/$USER && git clone https://github.com/MichaelSebero/PrivateGPT4Linux &&
 GPT4ALL="curl -LO https://gpt4all.io/models/ggml-gpt4all-j-v1.3-groovy.bin"
 ENV="curl -LO https://raw.githubusercontent.com/MichaelSebero/PrivateGPT4Linux-Installer/main/env/.env"
 
-mkdir /home/models && cd /home/models && eval "$GPT4ALL" && cd /home/$USER/PrivateGPT4Linux && eval "$ENV" && pip install -r requirements.txt && chmod 777 /home/$USER/PrivateGPT4Linux/.env && chmod 777 /home/models -R && chmod 777 /usr/lib/python3.11/site-packages -R'
+mkdir /home/models && cd /home/models && eval "$GPT4ALL" && cd /home/$USER/PrivateGPT4Linux && eval "$ENV" && pip install -r requirements.txt && chmod 777 /home/$USER/PrivateGPT4Linux/.env && chmod 777 /home/models -R'
+
+cd /home/user/PrivateGPT4Linux && sudo pip install -r requirements.txt
