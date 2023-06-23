acdf017d682a81e6fbb35a399c94c37557f32496
Michael Sebero <106393611+MichaelSebero@users.noreply.github.com>
2023-06-23 13:44:47 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,4 +6,4 @@ git clone https://github.com/imartinez/privateGPT && cd /home/$USER/privateGPT &
 GPT4ALL="curl -LO https://gpt4all.io/models/ggml-gpt4all-j-v1.3-groovy.bin"
 ENV="curl -LO https://raw.githubusercontent.com/MichaelSebero/PrivateGPT4Linux/main/.env"
 
-mkdir /home/models && cd /home/models && eval "$GPT4ALL" && cd /home/$USER/privateGPT && eval "$ENV" && chmod 777 /home/$USER/privateGPT/.env && chmod 777 /home/models -R'
+mkdir /home/models && cd /home/models && eval "$GPT4ALL" && cd /home/$USER/privateGPT && eval "$ENV" && chmod 777 /home/$USER/privateGPT/.env && rm /home/user/privateGPT/source_documents/state_of_the_union.txt && chmod 777 /home/models -R'
