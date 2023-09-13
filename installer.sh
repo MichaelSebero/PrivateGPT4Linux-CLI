74c8e53ead5223bfe24602b648980116a045a8e8
Michael Sebero <106393611+MichaelSebero@users.noreply.github.com>
2023-09-13 17:33:21 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 
 # License: GNU GPLv3
 
-git clone https://github.com/imartinez/privateGPT && cd /home/$USER/privateGPT && pip install -r requirements.txt --break-system-packages && su -c '
+git clone https://github.com/imartinez/privateGPT && cd /home/$USER/privateGPT && pip install -r requirements.txt --break-system-packages --user && su -c '
 GPT4ALL="curl -LO https://gpt4all.io/models/ggml-gpt4all-j-v1.3-groovy.bin"
 ENV="curl -LO https://raw.githubusercontent.com/MichaelSebero/PrivateGPT4Linux/main/.env"
 
